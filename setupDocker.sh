411152fc2e15235659947356a5be999e09766de4
guepy <www.guepy@yahoo.com>
2022-09-02 11:48:38 +0100

    Last commit before I left ECA lab 2

--- setupDocker.sh
+++ setupDocker.sh
@@ -1,13 +1,6 @@
  #!/bin/sh
  set -x
  echo "start"
- echo "creating folder rfid project..."
- mkdir -p ~/rfidproject
- echo "[OK]"
- echo "cd to folder rfid project..."
- cd ~/rfidproject
- echo "[OK]"
- 
  echo "Updating packages database..."
  sudo apt update 
  sudo apt upgrade -y
@@ -23,28 +16,23 @@
  pip install pybluez
  
  echo "[OK]"
- echo "Downloading docker setup file..."
  if [ -f "./get-docker.sh" ]
  then 
-	echo "file already exist"
- else
-	curl -fsSL https://get.docker.com -o get-docker.sh
-	chmod +x ./get-docker.sh
 	echo "Installing docker tools..."
 	sudo sh ./get-docker.sh
+ else
+	echo "Make sure docker is install on your host"
  fi
  echo "[OK]"
  sudo usermod -aG docker ${USER}
  echo "[OK]"
- echo "Clonnig docker compose directory..."
  if [ -d "./compose" ]
  then
-	echo "Directory compose already exist"
- else 
-	git clone https://github.com/docker/compose.git
  echo "Installing docker compose..."
  cd ./compose
  make
+ else 
+	echo "Make sure docker compose is install on your host"
  fi
  echo "[OK]"
  cd ..
@@ -52,7 +40,6 @@
  #sudo sh -c "echo { \"insecure-registries\":[\"172.16.3.80:5000\"] } >> /etc/docker/daemon.json"
  echo "export DOCKER_BUILDKIT=1" >> ~/.bashrc
  echo "export COMPOSE_DOCKER_CLI_BUILD=0" >> ~/.bashrc
- python3 parse_mac_addr.py
  source ~/.bashrc
  #adresse du broker kafka
  sudo sh -c "echo \"195.221.0.168 sun-si-pluss\" >> /etc/hosts"
@@ -61,9 +48,7 @@
  then
 	rm -Rfa ./pyBluez
  fi
- git clone https://github.com/guepy/pybluez_rpi.git pyBluez
  sudo chown -R ${USER}:${USER} .
- cd pyBluez
  docker compose build
  docker compose up -d
  echo "[Finished]"
